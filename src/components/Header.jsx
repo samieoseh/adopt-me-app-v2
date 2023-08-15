f81886bcf7998f44d5388586acc0b401abf41c46
Samuel Oseh <samieoseh@gmail.com>
2023-08-15 22:55:33 +0000

    Aug 15, 2023, 11:55 PM

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,30 @@
+import styles from "./styles/Header.module.css";
+import favIcon from "../assets/svg/favorite.svg";
+import filterIcon from "../assets/svg/filter.svg";
+
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <header>
+            <h1>Logo</h1>
+            <nav>
+                <ul className={styles.navLinks}>
+                    <li>
+                        <a>Cat</a>
+                    </li>
+                    <li>
+                        <a>Dog</a>
+                    </li>
+                </ul>
+
+                <div className={styles.favorite}>
+                    <img src={favIcon} alt="favorite" />
+                </div>
+                <div className={styles.filter}>
+                    <img src={filterIcon} alt="favorite" />
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
